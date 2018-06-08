15961d69d1e30438bd4d29d5d5812a45c37e0e88
Allen White <awhite@captechventures.com>
2018-06-08 17:11:58 -0400

    Layout top header for acct deets

---
+++ SiriBanker/SiriBanker/AccountDetails/AccountDetailsViewController.swift
@@ -0,0 +1,37 @@
+//
+//  AccountDetailsViewController.swift
+//  SiriBanker
+//
+//  Created by Allen White on 6/8/18.
+//  Copyright © 2018 LBrands. All rights reserved.
+//
+
+import UIKit
+
+class AccountDetailsViewController: UIViewController {
+
+	@IBOutlet weak var topBackgroundView: UIView!
+	@IBOutlet weak var accountTypeLabel: UILabel!
+	@IBOutlet weak var accountNumberLabel: UILabel!
+	@IBOutlet weak var accountBalanceLabel: UILabel!
+	@IBOutlet weak var accountBalanceDescLabel: UILabel!
+	
+	
+	override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
