d0b47f347db7480a9308e23db1b02d934eea16d1
Mark Broski <mark@mbroski.com>
2018-06-08 17:01:13 -0400

    update new files

--- SiriBanker/SiriBanker/AccountsSummary/AccountsSummaryTableViewCell.swift
+++ SiriBanker/SiriBanker/AccountsSummary/AccountsSummaryTableViewCell.swift
@@ -10,13 +10,12 @@ import UIKit
 
 class AccountsSummaryTableViewCell: UITableViewCell {
 
-	@IBOutlet weak var gradientView: UIView!
-	@IBOutlet weak var accountTypeLabel: UILabel!
-	@IBOutlet weak var accountBalanceLabel: UILabel!
-	@IBOutlet weak var accountNumberLabel: UILabel!
-	@IBOutlet weak var accountBalanceDescriptionLabel: UILabel!
-	
-	
+    @IBOutlet weak var gradientView: UIView!
+    @IBOutlet weak var accountTypeLabel: UILabel!
+    @IBOutlet weak var accountBalanceLabel: UILabel!
+    @IBOutlet weak var accountNumberLabel: UILabel!
+    @IBOutlet weak var accountBalanceDescriptionLabel: UILabel!
+
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -27,5 +26,4 @@ class AccountsSummaryTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-
 }
