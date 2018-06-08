81aeb021dcf156e8acd9c54f0d49495bea61a9e9
Mark Broski <mark@mbroski.com>
2018-06-08 19:58:21 -0400

    Adding SiriBankerKitFramework

---
+++ SiriBanker/SiriBankerKit/SiriBankerKit.h
@@ -0,0 +1,19 @@
+//
+//  SiriBankerKit.h
+//  SiriBankerKit
+//
+//  Created by Mark Broski on 6/8/18.
+//  Copyright © 2018 LBrands. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SiriBankerKit.
+FOUNDATION_EXPORT double SiriBankerKitVersionNumber;
+
+//! Project version string for SiriBankerKit.
+FOUNDATION_EXPORT const unsigned char SiriBankerKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SiriBankerKit/PublicHeader.h>
+
+
